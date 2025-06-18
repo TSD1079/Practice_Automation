b6f712c23a9314a2f884ff6c200e0fc9c6af336e
tamsingh <tamannasingh14178@gmail.com>
2025-06-18 10:21:34 +0530

    Initial commit

---
+++ src/Practice_Java/Triangle_L001.java
@@ -0,0 +1,14 @@
+package Practice_Java;
+
+import java.util.Scanner;
+
+public class Triangle_L001 {
+    public static void main(String[] args) {
+        Scanner a= new Scanner(System.in);
+        System.out.println("Enter day number");
+        int day = scanner.nextInt();
+        
+
+
+    }
+}
